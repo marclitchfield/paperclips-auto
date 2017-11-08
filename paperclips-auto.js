afcf4b096b8de0df9ff0261e309e12bd82953994
Marc Litchfield <marclitchfield@gmail.com>
2017-11-07 22:26:06 -0800

    Finished game rules

--- paperclips-auto.js
+++ paperclips-auto.js
@@ -1,142 +1,237 @@
 (function() {
 
+  const ACCEPT_OFFER = false;
+
   actions = [
     {
       description: 'add processor',
+      timeout: 100,
       control: 'btnAddProc',
-      condition: () => (val('processors') < val('memory')/2)
-                     || val('memory') >= 250
+      condition: () => exists('processorDisplay')
+        && (val('processors') < val('memory')/2 
+          || val('memory') >= 250)
     },
     {
       description: 'add memory',
+      timeout: 100,
       control: 'btnAddMem',
-      condition: () => val('memory') < 250
-    },    
+      condition: () => exists('memoryDisplay') && val('memory') < 250
+    },
     {
-      description: (control) => 'buy project: ' + control.querySelector('span').innerText,
-      control: () => document.querySelector('.projectButton:enabled'),
+      description: (control) => 'project: ' + control.querySelector('span').innerText,
+      control: () => [].find.call(document.querySelectorAll('.projectButton:enabled'), (p) => 
+        p.querySelector('span').innerText.trim() !== (ACCEPT_OFFER ? 'Reject' : 'Accept')) || {},
       condition: () => true
     },
     {
-      phase: 0,
+      description: '# buy wire',
+      phase: 1,
       control: 'btnBuyWire',
       condition: () => val('wire') === 0
     },
     {
-      phase: 0,
       description: 'expand marketing',
+      phase: 1,
       control: 'btnExpandMarketing',
       condition: () => val('funds') > val('adCost') + val('wireCost')
     },    
     {
-      phase: 0,
+      description: '# lower price',
+      phase: 1,
       timeout: 2000,
       control: 'btnLowerPrice',
       condition: () => val('unsoldClips') > Math.log10(val('clips'))*100 && val('margin') > 0.02
     },
     {
-      phase: 0,
+      description: '# raise price',
+      phase: 1,
       timeout: 2000,
       control: 'btnRaisePrice',
       condition: () => val('unsoldClips') < Math.log10(val('clips'))*10
-                    && val('wire') > 0 && val('margin') < 0.10
+        && val('wire') > 0 && val('margin') < 0.10
     },
     {
-      phase: 0,
       description: 'improve investments',
+      phase: 1,
       control: 'btnImproveInvestments',
       condition: () => exists('investmentEngine')
     },
     {
-      phase: 0,
       description: 'investment strategy',
+      phase: 1,
       control: 'investStrat',
       condition: () => exists('investmentEngine') && el('investStrat').selectedIndex != 2,
       perform: (control) => control.selectedIndex = 2     
     },
     {
-      phase: 0,
       description: 'withdraw',
+      phase: 1,
       control: 'btnWithdraw',
       condition: () => exists('investmentEngine') 
-                  && ((val('investmentBankroll') > 1000000 && val('secValue') > 0)
-                    || val('avgSales') === 0)
+        && ((val('investmentBankroll') > 1000000 && val('secValue') > 0)
+        || val('avgSales') === 0)
     },
     {
-      phase: 0,
       description: 'invest',
+      phase: 1,
       control: 'btnInvest',
       timeout: 10000,
       condition: () => exists('investmentEngine')
         && ((val('portValue') < 2000000
-        && val('funds') > 10000 && val('funds') < 1000000 
+        && val('funds') > 10000 
         && val('wire') > 3000) || val('portValue') === 0)
     },
     {
-      phase: 0,
       description: 'buy autoclippers',
+      phase: 1,
       control: 'btnMakeClipper',
       condition: () => val('clipmakerLevel2') < Math.min(val('marketingLvl')*10, 100)
-                    && val('funds') > val('clipperCost') + val('wireCost')
+        && val('funds') > val('clipperCost') + val('wireCost')
     },
     {
-      phase: 0,
       description: 'buy megaclippers',
+      phase: 1,
       control: 'btnMakeMegaClipper',
       condition: () => val('funds') > val('megaClipperCost') + val('wireCost')
+        && val('megaClipperLevel') < 105
     },    
     {
-      phase: 0,
+      description: '# make paperclip',
+      phase: 1,
       control: 'btnMakePaperclip',
       condition: () => val('clipmakerLevel2') < 20
     },
     {
-      phase: 1,
+      description: 'final paperclips...',
+      phase: 4,
+      control: 'btnMakePaperclip',
+      condition: () => true
+    },
+    {
       description: 'make solar farm',
+      phase: 2,
       control: 'btnMakeFarm',
       condition: () => val('powerConsumptionRate')+500 >= val('powerProductionRate')
     },
     {
-      phase: 1,
       description: 'make battery tower',
+      phase: 2,
       control: 'btnMakeBattery',
       condition: () => val('maxStorage') <= val('powerProductionRate')*100
     },
     {
-      phase: 1,
       description: 'make harvester',
-      control: 'btnMakeHarvester',
+      phase: 2,
       timeout: 2,
+      control: 'btnMakeHarvester',
       condition: () => ((val('powerConsumptionRate') < val('powerProductionRate')
-                    && val('harvesterLevelDisplay') <= val('factoryLevelDisplay')*500))
+        && val('harvesterLevelDisplay') <= val('factoryLevelDisplay')*200))
     },
     {
-      phase: 1,
-      timeout: 2,
       description: 'make wire drone',
+      phase: 2,
+      timeout: 2,
       control: 'btnMakeWireDrone',
       condition: () => val('powerConsumptionRate') < val('powerProductionRate')
-                    && val('wireDroneLevelDisplay') <= val('factoryLevelDisplay')*500
+        && val('wireDroneLevelDisplay') <= val('factoryLevelDisplay')*200
     },  
     {
-      phase: 1,
       description: 'make factory',
+      phase: 2,
       control: 'btnMakeFactory',
       condition: () => (val('powerConsumptionRate')+500 < val('powerProductionRate'))
-                     || val('factoryLevelDisplay') === 0
+        || val('factoryLevelDisplay') === 0
+    },
+    {
+      description: 'increase probe trust',
+      phase: 3,
+      control: 'btnIncreaseProbeTrust',
+      condition: () => true
+    },
+    {
+      description: 'increase max trust',
+      phase: 3,
+      control: 'btnIncreaseMaxTrust',
+      condition: () => true
+    },
+    {
+      description: 'raise probe speed',
+      phase: 3,
+      control: 'btnRaiseProbeSpeed',
+      condition: () => val('probeSpeedDisplay') < (val('probeTrustUsedDisplay')/10)
+    },
+    {
+      description: 'raise probe nav',
+      phase: 3,
+      control: 'btnRaiseProbeNav',
+      condition: () => val('probeNavDisplay') < (val('probeTrustUsedDisplay')/30)
+    },
+    {
+      description: 'raise probe rep',
+      phase: 3,
+      control: 'btnRaiseProbeRep',
+      condition: () => val('probeRepDisplay') < (val('probeTrustUsedDisplay')/4)
+    },
+    {
+      description: 'raise probe haz',
+      phase: 3,
+      control: 'btnRaiseProbeHaz',
+      condition: () => val('probeHazDisplay') < (val('probeTrustUsedDisplay')/4)
+    },
+    {
+      description: 'raise probe fac',
+      phase: 3,
+      control: 'btnRaiseProbeFac',
+      condition: () => val('probeFacDisplay') === 0
+    },
+    {
+      description: 'raise probe harv',
+      phase: 3,
+      control: 'btnRaiseProbeHarv',
+      condition: () => val('probeHarvDisplay') === 0
+    },
+    {
+      description: 'raise probe wire',
+      phase: 3,
+      control: 'btnRaiseProbeWire',
+      condition: () => val('probeWireDisplay') === 0
+    },
+    {
+      description: 'raise probe combat',
+      phase: 3,
+      control: 'btnRaiseProbeCombat',
+      condition: () => val('probeCombatDisplay') < (val('probeTrustUsedDisplay')/3)
+    },
+    {
+      description: 'launch probe',
+      phase: 3,
+      control: 'btnMakeProbe',
+      condition: () => val('probesTotalDisplay') === 0 && val('probeTrustUsedDisplay') >= 20
+    },
+    {
+      description: 'entertain swarm',
+      control: 'btnEntertainSwarm',
+      condition: () => exists('entertainButtonDiv'),
+    },
+    {
+      description: 'synchronize swarm',
+      control: 'btnSynchSwarm',
+      condition: () => exists('synchButtonDiv')
     },
     {
       description: 'pick strategy',
       control: 'stratPicker',
       condition: () => exists('tournamentManagement')
-                    && el('stratPicker').selectedIndex < el('stratPicker').querySelectorAll('option').length-1,
-      perform: (control) => control.selectedIndex = control.querySelectorAll('option').length - 1
+        && el('stratPicker').selectedIndex !== Math.min(6, el('stratPicker').querySelectorAll('option').length-1),
+      perform: (control) => control.selectedIndex = Math.min(6, control.querySelectorAll('option').length - 1)
     },
     {
+      description: '# run tournament',
       control: 'btnRunTournament',
       condition: () => exists('tournamentManagement')
     },
     {
+      description: '# new tournament',
       control: 'btnNewTournament',
       condition: () => exists('tournamentManagement')
     },
@@ -147,6 +242,7 @@
       perform: (control) => control.value = 150
     },    
     {
+      description: '# q compute',
       control: 'btnQcompute',
       condition: () => {
         var chips = document.querySelectorAll('.qChip');
@@ -178,9 +274,13 @@
 
   function detectPhase() {
     if (exists('businessDiv'))
-      return 0;
-    if (exists('powerDiv'))
       return 1;
+    if (exists('powerDiv'))
+      return 2;
+    if (exists('probeDesignDiv'))
+      return 3;
+    
+    return 4;
   }
 
   function updateActionPointer(control) {
@@ -212,26 +312,24 @@
       if (action.phase !== undefined && action.phase !== currentPhase)
         return false;
 
-      if (action.condition()) {
-        const control = typeof(action.control) === 'function' ? action.control() : el(action.control);
-        if (control !== null && enabled(control.id) && !skipForTimeout(action)) {
-          if (typeof(action.perform) === 'function') {
-            action.perform(control);
-          } else {
-            control.click();
-          }
-
-          updateActionPointer(control);
-
-          if (action.description) {
-            console.log(typeof(action.description) === 'function' 
-            ? action.description(control) : action.description);
-          }
-
-          return true;
+      const control = typeof(action.control) === 'function' ? action.control() : el(action.control);
+      if (control !== null && enabled(control.id) && !skipForTimeout(action) && action.condition(control)) {
+        if (typeof(action.perform) === 'function') {
+          action.perform(control);
+        } else {
+          control.click();
         }
+
+        updateActionPointer(control);
+
+        if (action.description && action.description.indexOf('#') !== 0) {
+          console.log(typeof(action.description) === 'function' 
+          ? action.description(control) : action.description);
+        }
+        return true;
       }
-    })
+      return false;
+    });
   }
 
   const actionPointer = document.createElement('div');
